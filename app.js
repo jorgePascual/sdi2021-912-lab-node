67d86306a7bce08dbe05a3c31cb067821f37e34e
jorgePascual <jorgepg05@gmail.com>
2021-03-29 03:20:33 +0200

    SDI-2021-912-7.1-Creación del proyecto Tienda Virtual.

---
+++ app.js
@@ -0,0 +1,20 @@
+// Módulos
+let express =require('express');
+let app =  express();
+
+// Variables
+app.set('port', 8081);
+
+app.get('/usuarios', function(req, res){
+    console.log("depurar aqui");
+    res.send('ver usuarios');
+});
+
+app.get('/canciones', function(req, res){
+    res.send('ver canciones');
+});
+
+// lanzar el servidor
+app.listen(app.get('port'), function(){
+    console.log('Servidor activo en puerto: ' + app.get('port'));
+});
